006453ad2b0a739f2fbe4e71b89b6b33ecc7be78
Angel J. Haro <haro.j.angel@gmail.com>
2024-06-20 18:43:03 -0700

    date utility and global css update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,34 @@
 import React from "react";
 
-import './assets/global.css';
+import "./assets/global.css";
 import { ThemeProvider, createGlobalStyle } from "styled-components";
 import { Landing } from "./pages/Landing";
 import { Theme } from "./utils/GlobalInterfaces";
 
-const theme:Theme= {
-  colors:
-  {
-    blue: '#1da1f2',
+const theme: Theme = {
+  colors: {
+    blue: "#1da1f2",
     black: "#14171a",
     darkGray: "#657786",
     lightGray: "#aab8c2",
     offWhite: "#e1e8ed",
-    white:"#f5f8fa",
-    error: "red"
-  }
-}
+    white: "#f5f8fa",
+    error: "red",
+  },
+};
 
-const GlobalStyle= createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
 *{
   font-family: "IBM Plex Sans", sans-serif;
   font-weight: 500;
 }
-`
+`;
 
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle/>
-      <Landing/>
+      <GlobalStyle />
+      <Landing />
     </ThemeProvider>
-  )
-}
+  );
+};
